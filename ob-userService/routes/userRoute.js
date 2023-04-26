f42e958db1d6d868a9b98a8220e9e7b68ef0c140
Kushan <kushanmigara@gmail.com>
2023-04-26 13:30:55 +0530

    user-service

---
+++ ob-userService/routes/userRoute.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+const {
+    createUser,
+    loginUserCtrl,
+    handleRefreshToken,
+    logout,
+   
+   
+} = require("../controller/userCtrl");
+
+
+
+
+router.post("/register",createUser)
+router.post("/logins", loginUserCtrl)
+router.get("/refresh",handleRefreshToken)
+router.get("/logout",logout)
+
+
+module.exports = router;
