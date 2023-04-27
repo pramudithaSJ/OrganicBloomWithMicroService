d79a6513d0747243a3f3052ce57712ba95166012
pramudithaSJ <IT20620202@my.sliit.lk>
2023-04-27 15:40:14 +0530

    backend and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./sharedComponent/login";
 
 function App() {
   return (
-    <div className="App bg-black h-screen">
-      <h1>sxs</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
