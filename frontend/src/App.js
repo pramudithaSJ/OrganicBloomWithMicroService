372273360d48e821e19e981ac3c4f6a9ae09c2d0
pramudithaSJ <IT20620202@my.sliit.lk>
2023-04-27 15:21:05 +0530

    tailwind configure

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App bg-black h-screen">
+      <h1>sxs</h1>
+    </div>
+  );
+}
+
+export default App;
