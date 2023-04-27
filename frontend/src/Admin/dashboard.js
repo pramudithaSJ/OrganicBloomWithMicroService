b27c83d9117ba52ba123993bde212613116da320
pramudithaSJ <IT20620202@my.sliit.lk>
2023-04-27 16:10:03 +0530

    admin side and seller side

---
+++ frontend/src/Admin/dashboard.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Sidebar() {
+  const [open, setOpen] = useState(false);
+  const [selectedField, setSelectedField] = useState("home");
+  let navigate = useNavigate();
+  return (
+    <div className="flex">
+      <div
+        className={` ${
+          open ? "w-40" : "w-60 "
+        } flex flex-col h-screen p-3 bg-gray-800 shadow duration-300`}
+      >
+        <div className="space-y-3">
+          <div className="flex items-center justify-between">
+            <h2 className="text-xl font-bold text-white">Dashboard</h2>
+            <button onClick={() => setOpen(!open)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div className="flex-1">
+            <ul className="pt-2 pb-4 space-y-1 text-sm">
+              <li className="rounded-sm">
+                <button
+                  onClick={() => setSelectedField("home")}
+                  className="flex items-center p-2 space-x-3 rounded-md hover:bg-slate-500 w-full"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-gray-100"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                    />
+                  </svg>
+                  <span className="text-gray-100">Orders</span>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto">
+        {selectedField === "instructors" ? <></> : null}
+      </div>
+    </div>
+  );
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./sharedComponent/login";
+import Sidebar from "./Admin/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Login />} />
+        <Route path="/dashboard" exact element={<Sidebar />} />
       </Routes>
     </BrowserRouter>
   );
