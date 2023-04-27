d75e5e277072ab8c4b1dbb3f54eb7ca64ccf2ac6
pramudithaSJ <IT20620202@my.sliit.lk>
2023-04-27 11:06:26 +0530

    database changed

--- backend/ob-gatewayService/middlewares/authMiddleware.js
+++ backend/ob-gatewayService/middlewares/authMiddleware.js
@@ -2,27 +2,44 @@ const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 
 const product = require("../models/productModel");
+const Token = require("../models/token");
 
 const authMiddleware = asyncHandler(async (req, res, next) => {
   let token;
   const secretKey = "mysecret";
+
   if (req.headers?.authorization.startsWith("Bearer")) {
-    token = req.headers.authorization.split(" ")[1];
-    console.log("Token", token);
+    token = req.headers.authorization.split(" ")[2];
+    console.log("Token ===", token);
     try {
       if (token) {
-        const decoded = jwt.verify(token, secretKey);
-        console.log("decode", decoded);
-        const user = await product.findById(decoded?.id);
-        req.user = user;
+        // const decoded = jwt.veify(token, secretKey);
+        // console.log("decode", decoded);
+
+        // Check if token exists in the database
+        const tokenDoc = await Token.findOne({ token });
+
+        if (!tokenDoc) {
+          throw new Error(
+            "Not Authorized. Token does not exist in the database."
+          );
+        }
+
+        // Check if the token belongs to a valid user
+        // const user = await product.findById(decoded?.id);
+        if (!user) {
+          throw new Error("Not Authorized. User does not exist.");
+        }
+
+        // req.user = user;
         next();
       }
     } catch (error) {
       console.log(error);
-      throw new Error("Not Authorized token expired , please login again");
+      throw new Error("Not Authorized. Token expired, please login again.");
     }
   } else {
-    throw new Error("There is no token attached to header");
+    throw new Error("Not Authorized. There is no token attached to header.");
   }
 });
 
--- backend/ob-gatewayService/server.js
+++ backend/ob-gatewayService/server.js
@@ -2,14 +2,26 @@ const express = require("express");
 const proxy = require("express-http-proxy");
 const cors = require("cors");
 const authMiddleware = require("./middlewares/authMiddleware");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const URL =
+  "mongodb+srv://itpproject2022:vMKyhwZ9nb2qt5Di@organicbloom.1zbkjaw.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("mongoDB connection successful !!!");
+});
 // Use the authentication middleware before proxying requests
 app.use("/user", authMiddleware, proxy("http://localhost:8080"));
-app.use("/product", proxy("http://localhost:8010"));
+app.use("/product", authMiddleware, proxy("http://localhost:8050"));
 
 app.listen(3030, () => {
   console.log("Gateway is running on port 3030");
